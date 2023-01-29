cb68db95fb7a9e648c8d3648e6195a654a85a0d9
NickCheng0921 <nickshacheng.0921@gmail.com>
2023-01-29 00:09:04 -0600

    Working on flask form

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask import request
+import sys 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/sendRequest', methods=['POST'])
+def sendRequest():
+    title = request.form['titleInput']
+    print(f'Request function was called w title {title} ! ', file=sys.stderr)
+    return ('', 204)
